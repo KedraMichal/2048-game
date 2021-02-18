25b6ade3d5325c1c1d78e2def9ef18f2fdbc3006
ked-m <ked-mic@wp.pl>
2021-02-18 21:59:36 +0100

    Initial commit

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
 from flask import Flask, render_template, request
 import game as ga
+import copy
 
 app = Flask(__name__)
-
+score = 0
 
 @app.route('/')
 @app.route('/main')
 def main():
     ga.start_board = [[2, 2, 4, 4], [0, 4, 2, 2], [0, 2, 2, 0], [0, 2, 0, 2]]
-    return render_template('main.html', board=ga.start_board, continue_game=True)
+    global score
+    score = 0
+    return render_template('main.html', board=ga.start_board, continue_game=True, score=score)
 
 
 @app.route('/move')
 def move():
+    global score
     chosen_direction = request.args.get('direction', None)
+    actual_board = copy.deepcopy(ga.start_board)
+
     if chosen_direction == "left":
         ga.merge_left(ga.start_board)
     elif chosen_direction == "right":
@@ -23,14 +29,18 @@ def move():
     elif chosen_direction == "down":
         ga.merge_down(ga.start_board)
 
-    add = ga.random_spawn(ga.start_board)
+    if actual_board != ga.start_board:
+        score += 10
+        add = ga.random_spawn(ga.start_board)
+    else:
+        add = True
 
     if add is False:
         continue_game = False
-        return render_template('main.html', gameover="Game over!", board=ga.start_board, continue_game=continue_game)
+        return render_template('main.html', gameover="Game over!", board=ga.start_board, continue_game=continue_game, score=score)
 
     continue_game = True
-    return render_template('main.html', board=ga.start_board, continue_game=continue_game)
+    return render_template('main.html', board=ga.start_board, continue_game=continue_game, score=score)
 
 
 if __name__ == "__main__":
